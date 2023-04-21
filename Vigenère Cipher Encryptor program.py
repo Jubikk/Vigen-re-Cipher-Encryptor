5abfa0c0669e44c8330a5a99f755347facd5117e
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-04-21 12:49:37 +0800

    Fifth commit

--- Vigenère Cipher Encryptor program.py
+++ Vigenère Cipher Encryptor program.py
@@ -1,5 +1,6 @@
 #Joebeck Adndrew F. Gusi | BSCPE 1-5 | Assignment No.3 |
 
+
 import tkinter as tk
 import time
 from PIL import Image, ImageTk
@@ -52,3 +53,35 @@ def encrypt_and_display():
 root = tk.Tk()
 root.title("Vigenere Cipher by Gusi")
 root.geometry("400x150")
+
+#Url/Image
+url = "https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+response = requests.get(url)
+img_data = response.content
+img = Image.open(io.BytesIO(img_data))
+tk_image = ImageTk.PhotoImage(img)
+
+background_label = tk.Label(root, image=tk_image)
+background_label.place(x=0, y=0, relwidth=1,relheight=1)
+
+plaintext_label = tk.Label(root, text="Enter your plaintext:")
+plaintext_entry = tk.Entry(root, width= 50)
+keyword_label = tk.Label(root,width=20, text="Enter your keyword:")
+keyword_entry = tk.Entry(root)
+encrypt_button = tk.Button(root, text="Encrypt", command=encrypt_and_display)
+
+
+ciphertext_label = tk.Label(root, text="")
+ciphertext_label['bg'] = 'orange'
+
+plaintext_label.pack()
+plaintext_entry.pack()
+keyword_label.pack()
+keyword_entry.pack()
+encrypt_button.pack()
+ciphertext_label.pack()
+
+root.resizable(width=True, height=True)
+
+root.mainloop()
+
