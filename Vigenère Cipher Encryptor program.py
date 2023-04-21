eafb1ec95504206e2df4c3ebf08a1cccbad702de
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-04-21 12:45:01 +0800

    Third commit

--- Vigenère Cipher Encryptor program.py
+++ Vigenère Cipher Encryptor program.py
@@ -16,3 +16,22 @@ def vigenere_table():
             row.append((i+j)%26)
         table.append(row)
     return table
+
+def encrypt_vigenere(plaintext, keyword):
+    table = vigenere_table()
+
+    # To convert plaintext and keyword to uppercase and remove spaces
+    plaintext = plaintext.upper().replace(" ", "")
+    keyword = keyword.upper()
+
+    #To match the len of the plaintext
+    keyword = keyword * (len(plaintext) // len(keyword) + 1)
+    keyword = keyword[:len(plaintext)]
+
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        row = ord(plaintext[i]) - ord("A")
+        col = ord(keyword[i]) - ord("A")
+        ciphertext += chr(table[row][col] + ord("A"))
+
+    return ciphertext
