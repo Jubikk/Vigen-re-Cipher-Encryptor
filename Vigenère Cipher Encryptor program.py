c023d9b739ecf80e45291685a6dac85381ba0276
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-04-21 12:43:55 +0800

    Second commit

--- Vigenère Cipher Encryptor program.py
+++ Vigenère Cipher Encryptor program.py
@@ -1,2 +1,18 @@
 #Joebeck Adndrew F. Gusi | BSCPE 1-5 | Assignment No.3 |
 
+import tkinter as tk
+import time
+from PIL import Image, ImageTk
+import requests
+import io
+from io import BytesIO
+
+# First we define vigenere_table
+def vigenere_table():
+    table = []
+    for i in range(26):
+        row = []
+        for j in range(26):
+            row.append((i+j)%26)
+        table.append(row)
+    return table
